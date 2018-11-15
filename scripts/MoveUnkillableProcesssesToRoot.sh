8a5a0178fbce9ba91c766696c64dd35e24a62755
Yecheng Fu <cofyc.jackson@gmail.com>
2018-11-15 17:14:00 +0800

    Detect kubelet systemd cgroup path automatically.

--- scripts/MoveUnkillableProcesssesToRoot.sh
+++ scripts/MoveUnkillableProcesssesToRoot.sh
@@ -6,7 +6,7 @@
 # This script is used to move unkillable processes to root cgroup.
 #
 
-pids=$(cat /sys/fs/cgroup/systemd/system.slice/kubelet.service/tasks | xargs -n 1 -Itaskid bash -c "echo -n taskid '   '; awk -F: '/^State:/ {print $2}' /proc/taskid/status" | grep -P 'D\s+\(disk sleep\)' | awk '{ print $1 }')
+set -e
 
 function movetoroot() {
     local pid=$1
@@ -15,6 +15,24 @@ function movetoroot() {
         echo $pid > /sys/fs/cgroup/$cgroup/cgroup.procs
     done
 }
+
+function find_kubelet_cgroup_path() {
+    local pid=$(ps -C kubelet -opid --no-headers | tr -d "[:space:]")
+    if [ -z "$pid" ]; then
+        echo "error: kubelet process is not found"
+	return 1
+    fi
+    local systemd_path=$(cat /proc/$pid/cgroup | awk -F: '/name=systemd/ {print $3}')
+    if [ -z "$systemd_path" ]; then
+        echo "error: kubelet systemd path is not found"
+	return 1
+    fi
+    echo "/sys/fs/cgroup/systemd${systemd_path}"
+}
+
+cgroup_path=$(find_kubelet_cgroup_path)
+echo "cgroup path: $cgroup_path"
+pids=$(cat $cgroup_path/tasks | xargs -n 1 -Itaskid bash -c "echo -n taskid '   '; awk -F: '/^State:/ {print $2}' /proc/taskid/status" | grep -P 'D\s+\(disk sleep\)' | awk '{ print $1 }')
 
 if [[ "$pids" == "" ]]; then
     echo "No processes found."
