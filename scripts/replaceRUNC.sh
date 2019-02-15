44d47255aa881475ca1849d9485f8c40474a4cc8
Yecheng Fu <cofyc.jackson@gmail.com>
2019-02-15 18:26:05 +0800

    Upgrade patch script to aea228ca659df702e142f7b064460f696b2d9480

--- scripts/replaceRUNC.sh
+++ scripts/replaceRUNC.sh
@@ -4,5 +4,5 @@ tmpdir=$(mktemp -d)
 trap "rm -r $tmpdir" EXIT
 
 cd $tmpdir
-wget -q https://raw.githubusercontent.com/cofyc/CVE/9d6be0d5d00a037ae8fa201ae2cd4dc31ded7750/CVE-2019-5736/patch_runc.sh
+wget -q https://raw.githubusercontent.com/cofyc/CVE/aea228ca659df702e142f7b064460f696b2d9480/CVE-2019-5736/patch_runc.sh
 bash patch_runc.sh
